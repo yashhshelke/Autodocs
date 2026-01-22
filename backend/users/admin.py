d8e8cb215c241bf0bd9e4cf4835e107568079d58
Yash Shelke <yashshelke276@gmail.com>
2026-01-23 01:54:09 +0530

    a

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,23 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import UserProfile
 
-
-class UserProfileInline(admin.StackedInline):
-    """Inline admin for UserProfile"""
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = 'Profile'
-    fields = ['avatar', 'bio', 'phone', 'preferences', 'total_missions', 'completed_missions']
-    readonly_fields = ['total_missions', 'completed_missions']
-
-
-class UserAdmin(BaseUserAdmin):
-    """Extended User admin with profile"""
-    inlines = (UserProfileInline,)
-
-
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+# Register your models here.
